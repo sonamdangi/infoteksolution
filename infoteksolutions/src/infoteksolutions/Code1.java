909739ec164ddc8b29e9a82f6176f35f2205d80e
sonamdangi <42836307+sonamdangi@users.noreply.github.com>
2018-09-17 02:33:28 -0400

    hello code updated

--- infoteksolutions/src/infoteksolutions/Code1.java
+++ infoteksolutions/src/infoteksolutions/Code1.java
@@ -5,6 +5,7 @@ public class Code1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello code");
+		System.out.println("new hello code ");
 	}
 
 }
